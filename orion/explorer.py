155d3e8fcd188dad42ae52b4165abd49ab2443a9
Alexander Geiger <alex.geiger95@gmx.de>
2020-02-16 23:56:49 +0100

    add similarity check prototype

--- orion/explorer.py
+++ orion/explorer.py
@@ -1,9 +1,13 @@
 import json
 import logging
+import math
 import pickle
 from datetime import datetime
 
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+import similaritymeasures as sm
 from bson import ObjectId
 from bson.errors import InvalidId
 from gridfs import GridFS
@@ -11,6 +15,7 @@ from mlblocks import MLPipeline
 from mongoengine import connect
 from pip._internal.operations import freeze
 from pymongo.database import Database
+from scipy import stats
 
 from orion import model
 from orion.data import load_signal
@@ -170,13 +175,26 @@ class OrionExplorer:
 
     def end_datarun(self, datarun, events, status='done'):
         try:
-            for start, stop, score in events:
-                model.Event.insert(
-                    datarun=datarun,
-                    start_time=int(start),
-                    stop_time=int(stop),
-                    score=score
-                )
+            for i in range(len(events)):
+                if len(events[i])==3:
+                    model.Event.insert(
+                        datarun=datarun,
+                        start_time=int(events[i][0]),
+                        stop_time=int(events[i][1]),
+                        score=events[i][2]
+                    )
+                else:
+                    event = model.Event.insert(
+                        datarun=datarun,
+                        start_time=int(events[i][0]),
+                        stop_time=int(events[i][1]),
+                        score=events[i][2],
+                        tag=events[i][3]
+                    )
+                    model.Comment.insert(
+                        event=event,
+                        text='similarity matching'
+                    )
         except Exception:
             LOGGER.exception('Error storing datarun %s events', datarun.id)
             status = 'error'
@@ -234,7 +252,73 @@ class OrionExplorer:
 
         return events.merge(comments, how='inner', on='event_id')
 
-    def run_experiment(self, project, pipeline, dataset, user_id=None):
+    def find_pattern_in_sequence(self, pattern, sequence):
+        step_size_var = 60
+        step_size = max(1, len(pattern) // step_size_var)
+        i = 0
+        similarity_dtw = list()
+        pattern_data = np.zeros((len(pattern), 2))
+        pattern_data[:, 0] = np.arange(len(pattern))
+        pattern_data[:, 1] = pattern[:]
+        while i < len(sequence) - len(pattern):
+            sequence_data = np.zeros((len(pattern), 2))
+            sequence_data[:, 0] = np.arange(len(pattern))
+            sequence_data[:, 1] = sequence[i:i + len(pattern)]
+            dtw, _ = sm.dtw(pattern_data, sequence_data)
+            similarity_dtw = similarity_dtw + [dtw / len(pattern)]
+            i += step_size
+
+        s = stats.zscore(similarity_dtw)
+        below = pd.Series(s < -4)
+        index_below = np.argwhere(below)
+
+        for idx in index_below.flatten():
+            below[max(0, idx - max(1, len(pattern)//step_size)):min(idx + max(1, len(pattern)//step_size) + 1, len(below))] = True
+
+        shift = below.shift(1).fillna(False)
+        change = below != shift
+        index = below.index
+
+        starts = (index[below & change]).tolist()
+        ends = (index[~below & change]).tolist()
+        severity = [min(similarity_dtw[start:end]) for start, end in np.array([starts, ends]).T]
+
+        for i in range(len(starts)):
+            min_dist_index = similarity_dtw[starts[i]:ends[i]].index(min(similarity_dtw[starts[i]:ends[i]])) + starts[i]
+            starts[i] = min_dist_index * step_size
+            ends[i] = starts[i] + len(pattern)
+
+        if len(ends) == len(starts) - 1:
+            ends.append(len(sequence) - 1)
+
+        return np.array([starts, ends, severity]).T
+
+    def _merge_sequences(self, sequences, signal):
+
+        sorted_sequences = sorted(sequences, key=lambda entry: entry[0])
+        new_sequences = [[signal] + sorted_sequences[0]]
+
+        for sequence in sorted_sequences[1:]:
+            prev_sequence = new_sequences[-1]
+            if int(sequence[0]) <= int(prev_sequence[2]):
+                new_sequences[-1] = (prev_sequence[0], prev_sequence[1], max(prev_sequence[2], sequence[1]), prev_sequence[3])
+            else:
+                new_sequences.append([signal] + sequence)
+        return new_sequences
+
+    def get_sequences(self, sig):
+        tag_list = ['Problem', 'Normal', 'Previously seen', 'Investigate', 'Do not investigate',
+                    'Postpone']
+        dataruns_signal = model.Datarun.find(signal=sig)
+        events_problem = self._list(model.Event,
+                                    datarun__in=[datarun for datarun in dataruns_signal],
+                                    tag__in=tag_list)
+        if len(events_problem) > 0:
+            events_problem = np.array(events_problem[['start_time', 'stop_time', 'tag']])
+            events_problem = self._merge_sequences(events_problem.tolist(), sig)
+        return events_problem
+
+    def run_experiment(self, project, pipeline, dataset, user_id=None, similarity_check=False):
         project = project
         pipeline = self.get_pipeline(pipeline)
         dataset = self.get_dataset(dataset)
@@ -249,12 +333,35 @@ class OrionExplorer:
         mlpipeline = self.load_pipeline(pipeline)
         signals = model.Signal.find(dataset=dataset.id)
 
-        for signal in signals:
+        events_sim = []
 
-            data = self.load_signal(signal)
-            datarun = self.start_datarun(experiment, signal)
+        if similarity_check == 'all':
+            for sig in signals:
+                events_problem = self.get_sequences(sig)
+                if len(events_problem) > 0:
+                    events_sim.extend(events_problem)
+
+        elif isinstance(similarity_check, list):
+            if len(similarity_check) > 0:
+                for sig in similarity_check:
+                    signal = model.Signal.find(name=sig)[0]
+                    events_problem = self.get_sequences(signal)
+                    if len(events_problem) > 0:
+                        events_sim.extend(events_problem)
+
+        for signal in signals:
+            if similarity_check == 'same':
+                events_sim = []
+                events_problem = self.get_sequences(signal)
+                if len(events_problem) > 0:
+                    events_sim.extend(events_problem)
+
+            events_sim = np.array(events_sim)
 
             try:
+                data = self.load_signal(signal)
+                datarun = self.start_datarun(experiment, signal)
+
                 LOGGER.info("Fitting the pipeline")
                 mlpipeline.fit(data)
 
@@ -288,6 +395,26 @@ class OrionExplorer:
                             }
                             with self._fs.new_file(**kwargs) as f:
                                 pickle.dump(value, f)
+
+                events = events.tolist()
+
+                for anomaly in events_sim:
+                    data_pattern = self.load_signal(anomaly[0])
+                    pattern = data_pattern.loc[
+                        (data_pattern['timestamp'] >= int(anomaly[1])) & (data_pattern['timestamp'] <= int(anomaly[2])), [
+                            'value']]
+                    pattern = list(np.array(pattern).flatten())
+                    sequence = list(np.array(data['value']).flatten())
+                    similar_sequences = self.find_pattern_in_sequence(pattern, sequence)
+
+                    for similar_sequence in similar_sequences:
+                        for event in events:
+                            if max(0, min(data.iloc[int(similar_sequence[1])]['timestamp'], event[1]) - max(data.iloc[int(similar_sequence[0])]['timestamp'], event[0]) + 1) > 0:
+                                if len(event)==3 and event[2] < similar_sequence[2]:
+                                    continue
+                                else:
+                                    events.remove(event)
+                                    events.append([data['timestamp'][int(similar_sequence[0])], data['timestamp'][int(similar_sequence[1])], 0, anomaly[3], similar_sequence[2]])
 
                 status = 'done'
 
